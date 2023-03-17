b126f73319d3d8510ad2f922daa4e873493dcc20
zaima? <1206691662@outlook.com>
2023-03-18 05:53:48 +0800

    fix: returned when the timestamp is a time format

--- src/typeScript/formatDate.ts
+++ src/typeScript/formatDate.ts
@@ -13,16 +13,23 @@ export default function (timeStamp:string | number,symbol:string="-",time=false)
     if(!timeStamp) {
         return null;
     }
-  
-    const date = new Date(+timeStamp);
+
+    let date = null  as null | Date;
+
+    if(String(Number(timeStamp)) === 'NaN') {
+        date = new Date(timeStamp);
+    } else {
+        date = new Date(+timeStamp);
+    }
+
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2,'0');
     const day = date.getDate().toString().padStart(2,'0');
 
     if(time) {
-        const hour = date.getHours();
-        const min = date.getMinutes();
-        const seconds = date.getSeconds();
+        const hour = date.getHours().toString().padStart(2,'0');
+        const min = date.getMinutes().toString().padStart(2,'0');
+        const seconds = date.getSeconds().toString().padStart(2,'0');
        return year + symbol + month + symbol + day + " " + hour + ":" + min + ":" + seconds;
     } else {
         return year + symbol + month + symbol + day;
