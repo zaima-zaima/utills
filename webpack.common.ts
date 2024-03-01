4818f23554772a2185d7de2827e43e156341aec5
zaima-zaima <zaimawork@outlook.com>
2024-03-01 15:33:22 +0800

    取消strict模式以及去掉webpack配置中html模板主页的根路径 /

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,40 +1,42 @@
-import {Configuration} from "webpack";
+import { Configuration } from "webpack";
 import path from "path";
 import MiniCssExtractPlugin = require("mini-css-extract-plugin");
 import HtmlWebpackPlugin = require("html-webpack-plugin");
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
 export default {
-    entry:{
-        index:"./src/index.ts"
-    },
-    output:{
-        path: path.resolve(__dirname,"dist"),
-        filename:"./js/[name][chunkhash].js"
-    },
-    
-    module:{
-        rules:[
-            {
-                test:/\.ts$/,use:["babel-loader","ts-loader"]
-            },
-            {
-                test:/\.less/,use:[MiniCssExtractPlugin.loader,"css-loader","less-loader"]
-            }
-        ]
-    },
-    plugins:[
-       new CleanWebpackPlugin(),
-       new MiniCssExtractPlugin({
-        filename:"./css/[chunkhash].css"
-       }), 
-       new HtmlWebpackPlugin({
-            template:path.resolve(__dirname,"/public/index.html"),
-            chunks:["index"],
-            filename:"[name].html"
-        })
+  entry: {
+    index: "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "./js/[name][chunkhash].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.less/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+      },
     ],
-    resolve:{
-        extensions:[".ts",".js"]
-    }
-} as Configuration
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./css/[chunkhash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      chunks: ["index"],
+      filename: "[name].html",
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+} as Configuration;
